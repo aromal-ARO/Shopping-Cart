0bc02d1dcb8bf10e017d3c8e82e98db2cc620bea
Aromal Udayan <aromalu2021@gmail.com>
2025-01-02 00:30:49 +0530

    items added to server working

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 var hbs =require('express-handlebars')
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs', hbs.engine ({extname: 'hbs',defaultLayout: 'layout',layoutsDir:__dirname +'/views/layout/',partialsDir:__dirname +'/views/partials/'}));
+app.engine('hbs', hbs.engine ({extname: 'hbs',defaultLayout: 'layout',layoutsDir:__dirname +'/views/layout/',partialsDir:__dirname +'/views/partials/',}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,8 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
